f22bdc887646f601d64b7a5ee24c0fbef01d53c3
WillOli <william.ika@gmail.com>
2022-10-07 11:21:50 -0300

    conta bancaria- corrigir print nome.

---
+++ AgenciaBancaria.java
@@ -0,0 +1,162 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AgenciaBancaria {
+
+    static Scanner input = new Scanner(System.in);
+    static ArrayList<Conta> ContasBancarias;
+
+    public static void main(String[] args){
+        ContasBancarias = new ArrayList<Conta>();
+        operacoes();
+    }
+    public static void operacoes() {
+        System.out.println("----------------------------------------------------");
+        System.out.println("------------Bem vindos a nossa Agência--------------");
+        System.out.println("----------------------------------------------------");
+        System.out.println("**** Selecione uma operação que deseja realizar ****");
+        System.out.println("----------------------------------------------------");
+        System.out.println("|   Opção 1 - Criar Conta   |");
+        System.out.println("|   Opção 2 - Depositar     |");
+        System.out.println("|   Opção 3 - Sacar         |");
+        System.out.println("|   Opção 4 - Transferir    |");
+        System.out.println("|   Opção 5 - Listar        |");
+        System.out.println("|   Opção 6 - Sair          |");
+
+        int operacao = input.nextInt();
+
+        switch(operacao) {
+            case 1:
+                criarConta();
+                break;
+            case 2:
+                depositar();
+                break;
+            case 3:
+                sacar();
+                break;
+            case 4:
+                transferir();
+                break;
+            case 5:
+                listarContas();
+                break;
+            case 6:
+                System.out.println("Obrigado por usar nossa agência");
+                System.exit(0);
+
+            default:
+                System.out.println("Opção inválida");
+                operacoes();
+                break;
+
+        }
+
+    }
+
+    public static void criarConta() {
+        System.out.println("\nNome: ");
+        String nome = input.nextLine();
+
+        System.out.println("\nCPF: ");
+        String cpf = input.next();
+
+        System.out.println("\nEmail: ");
+        String email = input.next();
+
+        Pessoa pessoa = new Pessoa(nome, cpf, email);
+
+        Conta conta = new Conta(pessoa);
+
+        ContasBancarias.add(conta);
+        System.out.println("Sua conta foi criada com sucesso!");
+
+        operacoes();
+
+    }
+
+    private static Conta encontrarConta(int numeroConta) {
+        Conta conta = null;
+        if (ContasBancarias.size() > 0) {
+            for (Conta c : ContasBancarias) {
+                if (c.getNumeroConta() == numeroConta) {
+                conta = c;
+            }
+        }
+    }
+        return conta;
+    }
+
+    public static void depositar(){
+        System.out.println("Número da conta: ");
+        int numeroConta = input.nextInt();
+
+        Conta conta = encontrarConta(numeroConta);
+
+        if(conta != null) {
+            System.out.println("Qual valor deseja depositar? ");
+            double valorDeposito = input.nextDouble();
+            conta.depositar(valorDeposito);
+            System.out.println("Valor depositado com sucesso! ");
+        }else{
+            System.out.println("Conta não encontrada! ");
+        }
+        operacoes();
+    }
+
+    public static void sacar(){
+        System.out.println("Número da conta");
+        int numeroConta = input.nextInt();
+
+        Conta conta = encontrarConta(numeroConta);
+
+        if(conta != null) {
+            System.out.println("Qual valor deseja sacar? ");
+            double valorSaque = input.nextDouble();
+            conta.sacar(valorSaque);
+           // System.out.println("Valor sacado com sucesso! ");
+        }else{
+            System.out.println("Conta não encontrada! ");
+        }
+        operacoes();
+    }
+
+    public static void transferir() {
+        System.out.println("Númeero da conta do remetente: ");
+        int numeroContaRemetente = input.nextInt();
+
+        Conta contaRemetente = encontrarConta(numeroContaRemetente);
+
+        if(contaRemetente != null) {
+            System.out.println("Número da conta destinatário: ");
+            int numeroContaDestinatario = input.nextInt();
+
+            Conta  contaDestinatario = encontrarConta(numeroContaDestinatario);
+
+            if(contaDestinatario != null){
+                System.out.println("Valor da transferência: ");
+                double valor = input.nextDouble();
+
+                contaRemetente.transferir(contaDestinatario, valor);
+            }else{
+                System.out.println("A conta para depósito não foi encontrada. ");
+            }
+        }else{
+            System.out.println("Conta para tranferência não encontrada. ");
+        }
+        operacoes();
+    }
+
+    public static void listarContas() {
+        if(ContasBancarias.size() > 0) {
+            for (Conta conta: ContasBancarias) {
+                System.out.println(conta);
+
+            }
+        }else{
+            System.out.println("Não há contas cadastradas!");
+        }
+        operacoes();
+    }
+
+}
---
+++ Conta.java
@@ -0,0 +1,68 @@
+public class Conta {
+    private static int contadorDeConta = 1;
+
+    private int numeroConta;
+    private Pessoa pessoa;
+    private double saldo = 0.0;
+
+    public Conta(Pessoa pessoa){
+        this.numeroConta = contadorDeConta;
+        this.pessoa = pessoa;
+        contadorDeConta += 1;
+    }
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String toString() {
+        return "\nNúmero da conta : " + this.getNumeroConta() +
+                "\nNome: " + this.pessoa.getNome() +
+                "\nCPF: " + this.pessoa.getCPF() +
+                "\nEmail: " + this.pessoa.getEmail() +
+                "\nSaldo: " + utils.doubleToString(this.getSaldo()) +
+                "\n";
+    }
+
+    public void depositar(double valor) {
+        if(valor > 0) {
+            setSaldo(getSaldo() + valor);
+            System.out.println("Seu depósito foi realizado com sucesso!");
+        }else{
+            System.out.println("Não foi possível realizar o depósito!");
+        }
+    }
+
+    public void sacar(double valor) {
+        if(valor > 0 && this.getSaldo() >= valor) {
+            setSaldo(getSaldo() - valor);
+            System.out.println("Saque realizado com sucesso!");
+        }else{
+            System.out.println("Não foi possível realizar o saque!");
+        }
+    }
+    public void transferir(Conta contaParadeposito, double valor) {
+        if(valor > 0 && this.getSaldo() >= valor){
+            setSaldo(getSaldo() - valor);
+            contaParadeposito.saldo = contaParadeposito.getSaldo() + valor;
+            System.out.println("Trânsferencia realizada com sucesso!");
+        }else{
+            System.out.println("Não foi possível realizar a transferência!");
+        }
+    }
+}
+
